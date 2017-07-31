9a38999cd5e81525e4271b943768d2637f3c34fd
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-07-31 15:25:57 -0400

    add store collection view to home page

---
+++ TAP/StoreHomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TAP
+//
+//  Created by Kiyoshi Woolheater on 7/28/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import UIKit
+
+class StoreHomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // initalize all outlets
+    @IBOutlet weak var storeCollectionView: UICollectionView!
+    
+    // initalize variables
+    var storeItems = ["Beer", "Wine", "Liquor", "Extras"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // TODO: initalize collection view
+    // create array of items
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.storeItems.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let reuseIdentifier = "cell"
+        let cell = storeCollectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! StoreCollectionViewCell
+        
+        cell.label.text = storeItems[indexPath.row]
+        cell.picture.image = #imageLiteral(resourceName: "Beer")
+        
+        return cell
+    
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // handle tap events
+        print("You selected cell \(indexPath.row)")
+    }
+    
+    
+    
+}
+
