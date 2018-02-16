9682d075c87f458fad6a5c8d900326daa19adf5d
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2018-02-15 22:03:09 -0500

    feat: Add Sign In View Controller and Create Account View Controller

---
+++ TAP/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  TAP
+//
+//  Created by Kiyoshi Woolheater on 2/15/18.
+//  Copyright © 2018 Kiyoshi Woolheater. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    // MARK: viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Button Actions
+    
+    @IBAction func createAccount(_ sender: Any) {
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+    }
+}
---
+++ TAP/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  TAP
+//
+//  Created by Kiyoshi Woolheater on 2/15/18.
+//  Copyright © 2018 Kiyoshi Woolheater. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    // MARK: viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Button action
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+    }
+    
+}
